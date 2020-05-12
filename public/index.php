b60a09b6a2371711be56cdb49f6f30410c7da749
Florian Valois <contact@florian-valois.com>
2020-05-12 13:05:01 +0200

    Reorganize files in public folder

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Futur en train</title>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet"> 
   <link rel="stylesheet" href="style.min.css">
   <script src="script.min.js"></script>
 </head>
