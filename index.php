441c777f87bcf681b4441ce99f096542d65e4f6b
Florian Valois <contact@florian-valois.com>
2020-05-12 12:27:22 +0200

    Update index.php
    - Add base HTML

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Futur en train</title>
+  <link rel="stylesheet" href="style.min.css">
+  <script src="script.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
